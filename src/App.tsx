da3a5b338ec69fe24e9cc278d4d642a5838f47f1
artlu99 <alu99.sm@gmail.com>
2024-02-04 08:25:06 -0500

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Catalog from 'components/Catalog'
 export default function () {
   const host: string = import.meta.env['VITE_HOST']
   const displayName: string = import.meta.env['VITE_HOST_DISPLAYNAME']
+  const githubLink: string = import.meta.env['VITE_GITHUB_LINK']
 
   return (
     <div className="container mx-auto max-w-prose p-10 prose">
@@ -12,10 +13,14 @@ export default function () {
         <Catalog />
         <hr />
         <h3>
-          hosted by{' '}
+          <a href={`${githubLink}`} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>{' '}
+          repository. Hosted by{' '}
           <a href={`${host}`} target="_blank" rel="noopener noreferrer">
             {`${displayName}`}
           </a>
+          .
         </h3>
       </Suspense>
     </div>
