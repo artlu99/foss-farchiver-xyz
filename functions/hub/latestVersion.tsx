21b041d041010fbc603c895a202f8120bd834994
artlu99 <alu99.sm@gmail.com>
2024-02-03 21:39:43 -0500

    reverse sort

--- functions/hub/latestVersion.tsx
+++ functions/hub/latestVersion.tsx
@@ -5,7 +5,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 
   const hubs: HubDetail[] = JSON.parse(await env.KV.get('hubs'))
 
-  const latest = hubs.sort((a, b) => (a.version > b.version ? 1 : -1))
+  const latest = hubs.sort((a, b) => (a.version > b.version ? -1 : 1))
 
   return new Response(JSON.stringify(latest[0].version))
 }
