ced7f5bc14d2b75d711ee021c62400eb647ddd94
artlu99 <alu99.sm@gmail.com>
2024-02-05 12:11:44 -0500

    refresh

--- functions/hubs/refresh.tsx
+++ functions/hubs/refresh.tsx
@@ -7,17 +7,24 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 
   const hubDetails = await Promise.all(
     knownHubs.map(async (h) => {
-      const url =
-        (h.ssl ? 'https' : 'http') + '://' + h.url + '/v1/info?dbstats=1'
+      if (h.ssl) {
+        // Cloudflare Workers do not allow https out to non-standard ports (such as 2281)
+        return h
+      } else {
+        const url =
+          (h.ssl ? 'https' : 'http') + '://' + h.url + '/v1/info?dbstats=1'
 
-      const res = (await (await fetch(url)).json()) as HubGetInfoResponse
-      const updatedHubDetail = {
-        ...h,
-        version: res.version,
+        const res = (await (await fetch(url)).json()) as HubGetInfoResponse
+        const updatedHubDetail = {
+          ...h,
+          version: res.version,
+        }
+        return updatedHubDetail
       }
-      return updatedHubDetail
     })
   )
 
-  return new Response(JSON.stringify({ hubDetails }))
+  await env.KV.put('hubs', JSON.stringify(hubDetails))
+
+  return new Response(JSON.stringify({ status: 'done' }))
 }
