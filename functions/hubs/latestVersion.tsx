b42d832427b216f2af23fa65c6929e1ee9d1b698
artlu99 <alu99.sm@gmail.com>
2024-02-11 22:49:55 -0500

    numberify dot version strings for sorting 1.9.9 and 1.10.0

--- functions/hubs/latestVersion.tsx
+++ functions/hubs/latestVersion.tsx
@@ -1,11 +1,18 @@
 import { Env, HubDetail } from '../types'
 
+const numberify = (dotVersion: string) => {
+  const [major, minor, sub] = dotVersion.split('.')
+  return Number(major) * 1000000 + Number(minor) * 1000 + Number(sub) * 1
+}
+
 export const onRequest: PagesFunction<Env> = async (context) => {
   const { request, env } = context
 
   const hubs: HubDetail[] = JSON.parse(await env.KV.get('hubs'))
 
-  const latest = hubs.sort((a, b) => (a.version > b.version ? -1 : 1))
+  const latest = hubs.sort((a, b) =>
+    numberify(a.version) > numberify(b.version) ? -1 : 1
+  )
 
   return new Response(JSON.stringify(latest[0].version))
 }
